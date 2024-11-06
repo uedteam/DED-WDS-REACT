cbbf9bf33be25206d21f12f8983179dcbcdcdceb
Kevin <kk2229.yang@gmail.com>
2024-11-07 00:04:00 +0800

    🆙 update :  modify checkbox component parameter

--- libs/src/ui/element/checkbox/checkbox.stories.tsx
+++ libs/src/ui/element/checkbox/checkbox.stories.tsx
@@ -1,8 +1,12 @@
 import { action } from '@storybook/addon-actions';
 import { Meta, StoryObj } from '@storybook/react';
-import { Checkbox, CheckboxProps } from './checkbox';
-import { useState } from 'react';
-import { Grid, Row, Column } from '@src/ui/section/grid';
+import { Checkbox } from './checkbox';
+
+const options = [
+  { label: '選項一', value: 'option1' },
+  { label: '選項二', value: 'option2' },
+  { label: '選項三', value: 'option3' },
+];
 
 export default {
   title: 'Component/Checkbox',
@@ -12,6 +16,7 @@ export default {
     themeColor: {
       description: '主題顏色',
       options: [
+        'none',
         'primary',
         'secondary',
         'tertiary',
@@ -33,28 +38,35 @@ export default {
         ],
       },
     },
-    className: {
-      description: '客製化樣式',
-    },
-    options: {
-      description: '選項',
+    dataSource: {
+      description: '資料來源',
     },
     direction: {
-      description: '方向',
+      description: '排列方向',
       control: {
         type: 'select',
         options: ['row', 'column'],
       },
     },
     initValue: {
-      description: '選中的選項',
+      description: '選中的項目',
+    },
+    className: {
+      description: '客製化樣式',
     },
     onChange: {
       description: '選中選項改變時的回調函數',
       action: 'onChange',
     },
   },
-  args: {},
+  args: {
+    themeColor: 'primary',
+    dataSource: options,
+    direction: 'row',
+    initValue: ['option1'],
+    className: '',
+    onChange: (e: string[]) => action('onChange')(e),
+  },
   parameters: {
     docs: {
       title: 'Checkbox',
@@ -66,106 +78,33 @@ export default {
 } as Meta;
 type Story = StoryObj<typeof Checkbox>;
 
-const options = [
-  { label: '選項一', value: 'option1' },
-  { label: '選項二', value: 'option2' },
-  { label: '選項三', value: 'option3' },
-];
-
-const DefaultWithHooks = (args: CheckboxProps) => {
-  const [value, setValue] = useState<string[]>(['option1']);
-
-  const handleChange = (e: string[]) => {
-    console.log(e);
-    setValue(e);
-  };
-
-  return (
-    <>
-      <Checkbox
-        {...args}
-        options={options}
-        initValue={value}
-        onChange={handleChange}
-      />
-      <div style={{ marginTop: '16px' }}>已選項目: [{value.join(', ')}]</div>
-    </>
-  );
-};
-
 export const Default: Story = {
   name: '預設項目',
-  args: {
-    options: options,
-    initValue: [],
-    onChange: (e) => action('onChange')(e),
-    direction: 'row',
-    className: '',
-  },
+  args: {},
   render(args) {
-    return <DefaultWithHooks {...args} />;
+    return <Checkbox {...args} />;
   },
-};
-
-const ThemeWithHooks = ({
-  themeColor,
-}: {
-  themeColor:
-    | 'primary'
-    | 'secondary'
-    | 'tertiary'
-    | 'success'
-    | 'warning'
-    | 'error'
-    | 'info';
-}) => {
-  const [value, setValue] = useState<string[]>(['option1']);
-
-  return (
-    <Checkbox
-      themeColor={themeColor}
-      options={options}
-      initValue={value}
-      onChange={setValue}
-    />
-  );
 };
 
 export const Theme: Story = {
   name: '主題色彩',
   args: {
-    options: options,
+    dataSource: options,
     initValue: ['option1'],
     onChange: (e) => action('onChange')(e),
     className: '',
   },
   render(args) {
     return (
-      <Grid>
-        <Row>
-          <Column xs={12} md={4}>
-            <ThemeWithHooks themeColor="primary" />
-          </Column>
-          <Column xs={12} md={4}>
-            <ThemeWithHooks themeColor="secondary" />
-          </Column>
-          <Column xs={12} md={4}>
-            <ThemeWithHooks themeColor="tertiary" />
-          </Column>
-          <Column xs={12} md={4}>
-            <ThemeWithHooks themeColor="info" />
-          </Column>
-          <Column xs={12} md={4}>
-            <ThemeWithHooks themeColor="success" />
-          </Column>
-          <Column xs={12} md={4}>
-            <ThemeWithHooks themeColor="warning" />
-          </Column>
-          <Column xs={12} md={4}>
-            <ThemeWithHooks themeColor="error" />
-          </Column>
-        </Row>
-      </Grid>
+      <div style={{ display: 'flex', flexDirection: 'column', gap: '16px' }}>
+        <Checkbox {...args} themeColor="primary" />
+        <Checkbox {...args} themeColor="secondary" />
+        <Checkbox {...args} themeColor="tertiary" />
+        <Checkbox {...args} themeColor="info" />
+        <Checkbox {...args} themeColor="success" />
+        <Checkbox {...args} themeColor="warning" />
+        <Checkbox {...args} themeColor="error" />
+      </div>
     );
   },
 };
