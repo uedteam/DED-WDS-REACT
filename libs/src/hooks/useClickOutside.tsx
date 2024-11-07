d1ab61d7323d46da010ceae19ecb493dddc49e8c
Kevin <kk2229.yang@gmail.com>
2024-11-07 13:10:21 +0800

    ✨feature：add useClickOugside hook

---
+++ libs/src/hooks/useClickOutside.tsx
@@ -0,0 +1,21 @@
+import { RefObject, useEffect } from 'react';
+
+export const useClickOutside = (
+  ref: RefObject<HTMLElement>,
+  callback: () => void
+) => {
+  const handleClickOutside = (event: MouseEvent) => {
+    if (ref.current && !ref.current.contains(event.target as Node)) {
+      callback();
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+};
+
+export default useClickOutside;
