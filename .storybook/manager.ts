5f909718c18ab5faa6ec970b79096526f611a15d
Kevin <kk2229.yang@gmail.com>
2024-11-18 15:29:13 +0800

    🆙 update : modify storybook ui style

--- .storybook/manager.ts
+++ .storybook/manager.ts
@@ -10,7 +10,7 @@ addons.setConfig({
     brandUrl:
       'https://uedteam.github.io/DED-WDS_REACT/?path=/docs/changelog--docs',
     brandImage:
-      'https://hkese.net/cdn-cgi/image/width=1200,format=auto/sites/default/files/article_images/342-3.jpg',
+      'https://storage.googleapis.com/ded-wds-bucket/1730727784135.jpg',
     brandTarget: '_self',
     colorPrimary: '#00467c',
     colorSecondary: '#00467c',
