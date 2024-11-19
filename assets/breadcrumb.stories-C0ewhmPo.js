5e8dc85b6822f95ec2f79f54c403670188dffc34
Kevin <kk2229.yang@gmail.com>
2024-11-19 13:11:19 +0800

    Updates

--- assets/breadcrumb.stories-C0ewhmPo.js
+++
@@ -1,7 +0,0 @@
-import{j as n}from"./jsx-runtime-CkxqCPlQ.js";import{B as o}from"./breadcrumb-B-3FAsN7.js";import"./index-DJO9vBfz.js";import"./usePosition-CCkH-I2k.js";import"./index-URyNLXnw.js";const c=[{label:"首頁",href:"/"},{label:"第一層",href:"/first"},{label:"第二層",href:"https://tw.yahoo.com"},{label:"第n層",href:"/first/second/third/n"},{label:"當前頁"}],u={title:"Component/Breadcrumb",component:o,tags:["autodocs"],argTypes:{dataSource:{description:"資料來源",table:{category:"PROPS"}},className:{description:"客製化樣式",control:{type:"text"},table:{category:"PROPS"}}},args:{dataSource:c,className:""},parameters:{docs:{title:"麵包屑",description:{component:"麵包屑組件的呈現及說明。"}}}},r={name:"預設項目",args:{},render(s){return n.jsx(o,{...s})}};var e,t,a;r.parameters={...r.parameters,docs:{...(e=r.parameters)==null?void 0:e.docs,source:{originalSource:`{
-  name: '預設項目',
-  args: {},
-  render(args) {
-    return <Breadcrumb {...args} />;
-  }
-}`,...(a=(t=r.parameters)==null?void 0:t.docs)==null?void 0:a.source}}};const b=["Default"];export{r as Default,b as __namedExportsOrder,u as default};
