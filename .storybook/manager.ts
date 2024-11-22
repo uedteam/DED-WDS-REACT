2c3a69eb1088a85719fbe11ddfa80fbd0b70bbd9
Kevin <kk2229.yang@gmail.com>
2024-11-22 16:09:27 +0800

    🆙 update : modify logo

--- .storybook/manager.ts
+++ .storybook/manager.ts
@@ -10,7 +10,7 @@ addons.setConfig({
     brandUrl:
       'https://uedteam.github.io/DED-WDS_REACT/?path=/docs/changelog--docs',
     brandImage:
-      'https://storage.googleapis.com/ded-wds-bucket/1730727784135.jpg',
+      'https://storage.googleapis.com/ded-wds-bucket/AUO-wds-logo-react.png',
     brandTarget: '_self',
     colorPrimary: '#00467c',
     colorSecondary: '#00467c',
--- .storybook/theme.ts
+++ .storybook/theme.ts
@@ -9,7 +9,8 @@ export default create({
   brandTitle: 'DED WDS Storybook',
   brandUrl:
     'https://uedteam.github.io/DED-WDS_REACT/?path=/docs/changelog--docs',
-  brandImage: 'https://storage.googleapis.com/ded-wds-bucket/1730727784135.jpg',
+  brandImage:
+    'https://storage.googleapis.com/ded-wds-bucket/AUO-wds-logo-react.png',
   brandTarget: '_self',
 
   //
