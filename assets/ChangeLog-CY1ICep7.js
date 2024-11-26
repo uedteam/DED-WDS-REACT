4eac72cdfd11e24fc994259f7e27909087108c56
Kevin <kk2229.yang@gmail.com>
2024-11-26 14:36:09 +0800

    Updates

--- assets/ChangeLog-CY1ICep7.js
+++ assets/ChangeLog-CY1ICep7.js
@@ -1,4 +1,4 @@
-import{j as n}from"./jsx-runtime-DEdD30eg.js";import{useMDXComponents as r}from"./index-CcnH5Kt0.js";import{ae as a,am as m}from"./index-5WF7FhP5.js";import"./index-RYns6xqu.js";import"./iframe-HBogLqzS.js";import"../sb-preview/runtime.js";import"./index-sbqOYYIm.js";import"./index-BwO1pRjM.js";import"./index-DrFu-skq.js";const i=`## V1.2.5 (2024/11/13)
+import{j as n}from"./jsx-runtime-DEdD30eg.js";import{useMDXComponents as r}from"./index-CcnH5Kt0.js";import{ae as a,am as m}from"./index-BSPfd0jO.js";import"./index-RYns6xqu.js";import"./iframe-SA_tAvMJ.js";import"../sb-preview/runtime.js";import"./index-sbqOYYIm.js";import"./index-BwO1pRjM.js";import"./index-DrFu-skq.js";const i=`## V1.2.5 (2024/11/13)
 
 ✨ New Feature
 
