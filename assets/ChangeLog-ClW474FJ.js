4bb375dddf7f46ca579bfea4d4fec8edd5d8eb52
Kevin <kk2229.yang@gmail.com>
2024-11-27 22:09:16 +0800

    Updates

---
+++ assets/ChangeLog-ClW474FJ.js
@@ -0,0 +1,33 @@
+import{j as n}from"./jsx-runtime-DEdD30eg.js";import{useMDXComponents as a}from"./index-CcnH5Kt0.js";import{ae as m,am as r}from"./index-C6QFG5h5.js";import"./index-RYns6xqu.js";import"./iframe-ju1g_5z1.js";import"../sb-preview/runtime.js";import"./index-sbqOYYIm.js";import"./index-BwO1pRjM.js";import"./index-DrFu-skq.js";const i=`## V1.2.6 (2024/11/26)
+
+✨ New Feature
+
+- add dropdown component
+- add badge component (WIP)
+
+---
+
+🆙 Update
+
+- use avatar component to avatar group
+- modify compute position function
+- change img element to image component
+- add introduction path
+- modify component index
+- change class name
+- remove useless svg
+- remove inline style
+- modify dialog component parameter
+- modify list outline parameter
+- update component className default
+- modify brand url and style
+
+---
+
+🪲 Bug Fix
+`;function t(e){const o={h1:"h1",...a(),...e.components};return n.jsxs(n.Fragment,{children:[`
+`,`
+`,`
+`,n.jsx(m,{title:"Introduction/Changelog"}),`
+`,n.jsx(o.h1,{id:"changelog",children:"Changelog"}),`
+`,n.jsx(r,{children:i})]})}function x(e={}){const{wrapper:o}={...a(),...e.components};return o?n.jsx(o,{...e,children:n.jsx(t,{...e})}):t(e)}export{x as default};
---
+++ assets/slider.stories-CvpphQU7.js
@@ -0,0 +1,50 @@
+import{j as r}from"./jsx-runtime-DEdD30eg.js";import{a as d}from"./chunk-D5ZWXAHU-CGd0bvzi.js";import"./title-DjADW0si.js";import"./button-CZ9socs2.js";import"./checkbox-RCY9NRZS.js";import"./input-BZMFkDDL.js";import"./textarea-jggigcCy.js";import"./toggle-CEWviFwF.js";import"./radio-wVtPZKvI.js";import{S as i}from"./avatar-CZRMuJM3.js";import"./breadcrumb-DEhtghWx.js";import"./progress-circle-DxITm_MT.js";import"./progress-line-BS6M-A1S.js";import"./styled-7_Fd9YFd.js";import"./menu-BhwzE_Pi.js";import"./image-BUf_2wZ-.js";import"./index-RYns6xqu.js";import"./lodash-CcdATr5u.js";import"./auo-TMZ-BSoc.js";import"./check-BnJBHFpS.js";import"./useDialog-Bg9wgsHg.js";import"./offline-Ddz6ifNG.js";import"./index-sbqOYYIm.js";import"./arrow_down-DA-sWbvp.js";const z={title:"Component/Slider",component:i,tags:["autodocs"],argTypes:{themeColor:{description:"主題顏色",control:{type:"select",options:["primary","secondary","tertiary","success","warning","error","info"]},table:{category:"PROPS"}},max:{description:"最大值",table:{category:"PROPS"}},min:{description:"最小值",table:{category:"PROPS"}},label:{description:"顯示單位",table:{category:"PROPS"}},step:{description:"步進值",table:{category:"PROPS"}},initValue:{description:"初始值",table:{category:"PROPS"}},isDisabled:{description:"是否禁用",table:{category:"PROPS"}},className:{description:"客製化樣式",table:{category:"PROPS"}},onChange:{description:"值改變事件",action:"onChange",table:{category:"EVENTS"}}},parameters:{docs:{title:"滑桿",description:{component:"滑桿的呈現及說明。"}}},args:{themeColor:"primary",min:-100,max:100,step:1,initValue:0,label:"℃",isDisabled:!1,onChange:d("onChange"),className:""}},o={name:"預設項目",args:{},render(e){return r.jsx(i,{...e})}},t={name:"主題色彩",args:{min:0,max:100,step:1,initValue:10,label:"%"},parameters:{docs:{source:{code:`
+<Slider {...args} initValue={40} themeColor="primary" />
+<Slider {...args} initValue={50} themeColor="secondary" />
+<Slider {...args} initValue={60} themeColor="tertiary" />
+<Slider {...args} initValue={70} themeColor="info" />
+<Slider {...args} initValue={80} themeColor="success" />
+<Slider {...args} initValue={90} themeColor="warning" />
+<Slider {...args} initValue={100} themeColor="error" />
+`}}},render(e){return r.jsxs("div",{children:[r.jsx(i,{...e,initValue:40,themeColor:"primary"}),r.jsx(i,{...e,initValue:50,themeColor:"secondary"}),r.jsx(i,{...e,initValue:60,themeColor:"tertiary"}),r.jsx(i,{...e,initValue:70,themeColor:"info"}),r.jsx(i,{...e,initValue:80,themeColor:"success"}),r.jsx(i,{...e,initValue:90,themeColor:"warning"}),r.jsx(i,{...e,initValue:100,themeColor:"error"})]})}};var a,l,s;o.parameters={...o.parameters,docs:{...(a=o.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  name: '預設項目',
+  args: {},
+  render(args) {
+    return <Slider {...args} />;
+  }
+}`,...(s=(l=o.parameters)==null?void 0:l.docs)==null?void 0:s.source}}};var n,m,c;t.parameters={...t.parameters,docs:{...(n=t.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  name: '主題色彩',
+  args: {
+    min: 0,
+    max: 100,
+    step: 1,
+    initValue: 10,
+    label: '%'
+  },
+  parameters: {
+    docs: {
+      source: {
+        code: \`
+<Slider {...args} initValue={40} themeColor="primary" />
+<Slider {...args} initValue={50} themeColor="secondary" />
+<Slider {...args} initValue={60} themeColor="tertiary" />
+<Slider {...args} initValue={70} themeColor="info" />
+<Slider {...args} initValue={80} themeColor="success" />
+<Slider {...args} initValue={90} themeColor="warning" />
+<Slider {...args} initValue={100} themeColor="error" />
+\`
+      }
+    }
+  },
+  render(args) {
+    return <div>
+        <Slider {...args} initValue={40} themeColor="primary" />
+        <Slider {...args} initValue={50} themeColor="secondary" />
+        <Slider {...args} initValue={60} themeColor="tertiary" />
+        <Slider {...args} initValue={70} themeColor="info" />
+        <Slider {...args} initValue={80} themeColor="success" />
+        <Slider {...args} initValue={90} themeColor="warning" />
+        <Slider {...args} initValue={100} themeColor="error" />
+      </div>;
+  }
+}`,...(c=(m=t.parameters)==null?void 0:m.docs)==null?void 0:c.source}}};const A=["Default","ThemeColor"];export{o as Default,t as ThemeColor,A as __namedExportsOrder,z as default};
