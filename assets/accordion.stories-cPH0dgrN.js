d40acbc53630a1199b37f341cd2ccc5758acd6c2
Kevin <kk2229.yang@gmail.com>
2024-11-28 15:10:08 +0800

    Updates

--- assets/accordion.stories-cPH0dgrN.js
+++
@@ -1,9 +0,0 @@
-import{j as p}from"./jsx-runtime-DEdD30eg.js";import"./title-BwTyIEWm.js";import"./button-DKMUNfa-.js";import"./checkbox-RCY9NRZS.js";import"./input-BzeWqZWL.js";import"./textarea-jggigcCy.js";import"./toggle-CEWviFwF.js";import"./radio-wVtPZKvI.js";import{A as i}from"./accordion-O36GPtsd.js";import"./breadcrumb-CT4pz3w0.js";import"./progress-circle-DxITm_MT.js";import"./progress-line-BS6M-A1S.js";import"./styled-7_Fd9YFd.js";import"./menu-zBjolxxR.js";import"./image-CZs88QCg.js";import"./index-RYns6xqu.js";import"./lodash-CcdATr5u.js";import"./auo-TMZ-BSoc.js";import"./check-BnJBHFpS.js";import"./useDialog-D987K1Gx.js";import"./offline-Ddz6ifNG.js";import"./arrow_down-DA-sWbvp.js";import"./index-sbqOYYIm.js";const h={title:"Component/Accordion",component:i,tags:["autodocs"],argTypes:{className:{description:"客製化樣式",control:{type:"text"}}},parameters:{docs:{title:"Accordion",description:{component:"Accordion 的呈現及說明。"}}}},r={name:"預設項目",args:{className:""},render(e){return p.jsx(i,{...e})}};var o,t,m;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
-  name: '預設項目',
-  args: {
-    className: ''
-  },
-  render(args) {
-    return <Accordion {...args} />;
-  }
-}`,...(m=(t=r.parameters)==null?void 0:t.docs)==null?void 0:m.source}}};const k=["Primary"];export{r as Primary,k as __namedExportsOrder,h as default};
