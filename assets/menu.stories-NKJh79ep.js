11854cbb813f2c620128bfc837d7af255eca1a4d
Kevin <kk2229.yang@gmail.com>
2024-11-28 14:54:47 +0800

    Updates

--- assets/menu.stories-NKJh79ep.js
+++
@@ -1,7 +0,0 @@
-import{j as t}from"./jsx-runtime-DEdD30eg.js";import{A as o}from"./auo-TMZ-BSoc.js";import"./index-RYns6xqu.js";import{S as r}from"./home-49bMB0eq.js";import{M as n}from"./menu-BhwzE_Pi.js";import"./arrow_down-DA-sWbvp.js";import"./useDialog-Bg9wgsHg.js";const c=[{title:"Dashboard",prefix:t.jsx(r,{width:24,height:24}),path:"/dashboard"},{title:"Settings",prefix:t.jsx(r,{width:24,height:24}),path:"/settings",children:[{title:"Profile",prefix:t.jsx(o,{width:24,height:24}),path:"/settings/profile"},{title:"Account",prefix:t.jsx(o,{width:24,height:24}),path:"/settings/account"}]},{title:"Help",prefix:t.jsx(r,{width:24,height:24}),path:"/help"}],x={title:"Component/Menu",component:n,tags:["autodocs"],argTypes:{dataSource:{description:"資料來源",table:{category:"PROPS"}},isCollapsed:{description:"是否收合",table:{category:"PROPS"}},color:{description:"顏色",control:{type:"color"},table:{category:"PROPS"}},className:{description:"客製化樣式",table:{category:"PROPS"}}},args:{dataSource:c,isCollapsed:!1,color:"#000000",className:""},parameters:{docs:{title:"選單",description:{component:"選單的呈現及說明。"}}}},e={name:"預設項目",args:{},render(p){return t.jsx(n,{...p})}};var s,a,i;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:`{
-  name: '預設項目',
-  args: {},
-  render(args) {
-    return <Menu {...args} />;
-  }
-}`,...(i=(a=e.parameters)==null?void 0:a.docs)==null?void 0:i.source}}};const S=["Default"];export{e as Default,S as __namedExportsOrder,x as default};
