4f5dde5072e3158d54c6f31b115397ea5ac9504e
Kevin <kk2229.yang@gmail.com>
2024-11-29 10:47:04 +0800

    Updates

---
+++ assets/accordion.stories-D1mOT9zU.js
@@ -0,0 +1,11 @@
+import{j as c}from"./jsx-runtime-DEdD30eg.js";import"./title-DjADW0si.js";import"./button-DKMUNfa-.js";import"./checkbox-RCY9NRZS.js";import"./input-BzeWqZWL.js";import"./textarea-jggigcCy.js";import"./toggle-CEWviFwF.js";import"./radio-wVtPZKvI.js";import{A as a}from"./accordion-4V9uErG7.js";import"./breadcrumb-CT4pz3w0.js";import"./progress-circle-DxITm_MT.js";import"./progress-line-BS6M-A1S.js";import"./styled-7_Fd9YFd.js";import"./menu-zBjolxxR.js";import"./image-BUf_2wZ-.js";import"./index-RYns6xqu.js";import"./lodash-CcdATr5u.js";import"./auo-TMZ-BSoc.js";import"./check-BnJBHFpS.js";import"./useDialog-D987K1Gx.js";import"./offline-Ddz6ifNG.js";import"./arrow_down-DA-sWbvp.js";import"./index-sbqOYYIm.js";const M={title:"Component/Accordion",component:a,tags:["autodocs"],argTypes:{dataSource:{description:"資料來源",control:{type:"object"}},isOpenAll:{description:"是否全部展開",control:{type:"boolean"}},className:{description:"客製化樣式",control:{type:"text"}}},parameters:{docs:{title:"Accordion",description:{component:"Accordion 的呈現及說明。"}}}},s=[{id:"1",label:"Q1. 什麼是 React？",detail:"React 是一個用於構建用戶界面的 JavaScript 函數庫。"},{id:"2",label:"Q2. React 的特點有哪些？",detail:"React 的特點有：組件化、單向數據流、虛擬 DOM。"},{id:"3",label:"Q3. React 的生態系統有哪些？",detail:"React 的生態系統有：React Router、Redux、Axios。"},{id:"4",label:"Q4. React 的優點有哪些？",detail:"React 的優點有：組件化、單向數據流、虛擬 DOM。"}],t={name:"預設項目",args:{dataSource:s,isOpenAll:!1,className:""},render(i){return c.jsx(a,{...i})}};var r,o,e;t.parameters={...t.parameters,docs:{...(r=t.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  name: '預設項目',
+  args: {
+    dataSource: qaList,
+    isOpenAll: false,
+    className: ''
+  },
+  render(args) {
+    return <Accordion {...args} />;
+  }
+}`,...(e=(o=t.parameters)==null?void 0:o.docs)==null?void 0:e.source}}};const P=["Primary"];export{t as Primary,P as __namedExportsOrder,M as default};
