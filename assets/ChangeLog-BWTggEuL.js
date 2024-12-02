0623e16c9f4f90218df5d8fc3eb2b5319a735883
Kevin <kk2229.yang@gmail.com>
2024-12-02 15:33:24 +0800

    Updates

---
+++ assets/ChangeLog-BWTggEuL.js
@@ -0,0 +1,24 @@
+import{j as n}from"./jsx-runtime-DEdD30eg.js";import{useMDXComponents as r}from"./index-CcnH5Kt0.js";import{ae as m,am as a}from"./index-dtI75dI5.js";import"./index-RYns6xqu.js";import"./iframe-DTVFhVPW.js";import"../sb-preview/runtime.js";import"./index-sbqOYYIm.js";import"./index-BwO1pRjM.js";import"./index-DrFu-skq.js";const i=`## V1.2.7 (2024/12/2)
+
+✨ New Feature
+
+- add badge component
+
+---
+
+🆙 Update
+
+- refactor list component
+- refactor menu component
+- modify side nav component
+- modify accordion component
+
+---
+
+🪲 Bug Fix
+`;function e(o){const t={h1:"h1",...r(),...o.components};return n.jsxs(n.Fragment,{children:[`
+`,`
+`,`
+`,n.jsx(m,{title:"Introduction/Changelog"}),`
+`,n.jsx(t.h1,{id:"changelog",children:"Changelog"}),`
+`,n.jsx(a,{children:i})]})}function g(o={}){const{wrapper:t}={...r(),...o.components};return t?n.jsx(t,{...o,children:n.jsx(e,{...o})}):e(o)}export{g as default};
