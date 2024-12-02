854b6ef2dcc381bf5c199f8462398b9eebbbdcf6
Kevin <kk2229.yang@gmail.com>
2024-12-02 15:25:17 +0800

    🆙 update : modify accordion component

--- libs/src/ui/element/accordion/accordion-item.tsx
+++ libs/src/ui/element/accordion/accordion-item.tsx
@@ -20,26 +20,70 @@ export const AccordionItem: React.FC<AccordionProps> = ({
     setIsOpen(isOpenAll);
   }, [isOpenAll]);
 
+  useEffect(() => {
+    document.querySelectorAll('details').forEach((details) => {
+      details.addEventListener('toggle', () => {
+        const content = details.querySelector('summary + p');
+        if (details.open && content) {
+          content.animate(
+            [
+              { opacity: 0, height: 0 },
+              {
+                opacity: 1,
+                height: content.scrollHeight + 'px',
+              },
+            ],
+            {
+              duration: 500,
+              easing: 'ease-in-out',
+            }
+          );
+        }
+      });
+    });
+    return () => {
+      document.querySelectorAll('details').forEach((details) => {
+        details.removeEventListener('toggle', () => {
+          const content = details.querySelector('summary + p');
+          if (details.open && content) {
+            content.animate(
+              [
+                { opacity: 0, height: 0 },
+                { opacity: 1, height: content.scrollHeight + 'px' },
+              ],
+              {
+                duration: 500,
+                easing: 'ease-in-out',
+              }
+            );
+          }
+        });
+      });
+    };
+  }, []);
+
   return (
-    <details
-      onToggle={(e) => {
-        setIsOpen((e.target as HTMLDetailsElement).open);
-      }}
-      open={isOpen}
-      className={`accordion-item ${className}`}
-    >
-      <summary>
-        <span>{label}</span>
-        <div
-          className={`${
-            isOpen ? 'accordion-item-open' : 'accordion-item-close'
-          }`}
-        >
-          <ArrowDownIcon width={20} height={20} />
-        </div>
-      </summary>
-      <p>{content}</p>
-    </details>
+    <li className="accordion-item">
+      <details
+        onToggle={(e) => {
+          setIsOpen((e.target as HTMLDetailsElement).open);
+        }}
+        open={isOpen}
+        className={className}
+      >
+        <summary>
+          <span>{label}</span>
+          <div
+            className={`${
+              isOpen ? 'accordion-item-open' : 'accordion-item-close'
+            }`}
+          >
+            <ArrowDownIcon width={20} height={20} />
+          </div>
+        </summary>
+        <p>{content}</p>
+      </details>
+    </li>
   );
 };
 export default AccordionItem;
