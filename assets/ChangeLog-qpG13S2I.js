a2aee0567db6e6d7ba6e3d2e52677d7f0acacaf4
Kevin <kk2229.yang@gmail.com>
2024-12-03 15:39:35 +0800

    Updates

--- assets/ChangeLog-qpG13S2I.js
+++ assets/ChangeLog-qpG13S2I.js
@@ -1,4 +1,4 @@
-import{j as n}from"./jsx-runtime-DEdD30eg.js";import{useMDXComponents as r}from"./index-CcnH5Kt0.js";import{ae as a,am as i}from"./index-CpWMDfLa.js";import"./index-RYns6xqu.js";import"./iframe-Ci10AK_p.js";import"../sb-preview/runtime.js";import"./index-sbqOYYIm.js";import"./index-BwO1pRjM.js";import"./index-DrFu-skq.js";const s=`## V1.2.8 (2024/12/2)
+import{j as n}from"./jsx-runtime-DEdD30eg.js";import{useMDXComponents as r}from"./index-CcnH5Kt0.js";import{ae as a,am as i}from"./index-Bucv3MEy.js";import"./index-RYns6xqu.js";import"./iframe-B5oGuENR.js";import"../sb-preview/runtime.js";import"./index-sbqOYYIm.js";import"./index-BwO1pRjM.js";import"./index-DrFu-skq.js";const s=`## V1.2.8 (2024/12/2)
 
 ✨ New Feature
 
