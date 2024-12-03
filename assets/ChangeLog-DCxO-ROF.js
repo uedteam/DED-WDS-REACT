6c0dc25888d294b7718232857c8738a4447079f1
Kevin <kk2229.yang@gmail.com>
2024-12-03 16:51:19 +0800

    Updates

--- assets/ChangeLog-DCxO-ROF.js
+++ assets/ChangeLog-DCxO-ROF.js
@@ -1,4 +1,4 @@
-import{j as n}from"./jsx-runtime-DEdD30eg.js";import{useMDXComponents as r}from"./index-CcnH5Kt0.js";import{ae as a,am as i}from"./index-C5X6R9cv.js";import"./index-RYns6xqu.js";import"./iframe-qeDMt0_i.js";import"../sb-preview/runtime.js";import"./index-sbqOYYIm.js";import"./index-BwO1pRjM.js";import"./index-DrFu-skq.js";const s=`## V1.2.8 (2024/12/2)
+import{j as n}from"./jsx-runtime-DEdD30eg.js";import{useMDXComponents as r}from"./index-CcnH5Kt0.js";import{ae as a,am as i}from"./index-CMVjMU6g.js";import"./index-RYns6xqu.js";import"./iframe-GwU9KBH2.js";import"../sb-preview/runtime.js";import"./index-sbqOYYIm.js";import"./index-BwO1pRjM.js";import"./index-DrFu-skq.js";const s=`## V1.2.8 (2024/12/2)
 
 ✨ New Feature
 
