e710176dfb6edfdb7afc5aba23e8a4cdd12e20db
Kevin <kk2229.yang@gmail.com>
2024-12-03 15:39:15 +0800

    Updates

---
+++ assets/ChangeLog-DcO2Ipbm.js
@@ -0,0 +1,22 @@
+import{j as n}from"./jsx-runtime-DEdD30eg.js";import{useMDXComponents as r}from"./index-CcnH5Kt0.js";import{ae as a,am as i}from"./index-CpWMDfLa.js";import"./index-RYns6xqu.js";import"./iframe-Ci10AK_p.js";import"../sb-preview/runtime.js";import"./index-sbqOYYIm.js";import"./index-BwO1pRjM.js";import"./index-DrFu-skq.js";const s=`## V1.2.8 (2024/12/2)
+
+✨ New Feature
+
+---
+
+🆙 Update
+
+- sort storybook side feature
+- modify use outside hook
+- modify parameter ref to ref array
+- refactor avatar group component and use outside hook
+
+---
+
+🪲 Bug Fix
+`;function e(o){const t={h1:"h1",...r(),...o.components};return n.jsxs(n.Fragment,{children:[`
+`,`
+`,`
+`,n.jsx(a,{title:"Introduction/Changelog"}),`
+`,n.jsx(t.h1,{id:"changelog",children:"Changelog"}),`
+`,n.jsx(i,{children:s})]})}function l(o={}){const{wrapper:t}={...r(),...o.components};return t?n.jsx(t,{...o,children:n.jsx(e,{...o})}):e(o)}export{l as default};
