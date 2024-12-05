055b27ccf736bf10ac4e6fc94827b705c15693d4
Kevin <kk2229.yang@gmail.com>
2024-12-05 10:47:53 +0800

    Updates

--- assets/ChangeLog-CSkN-eH1.js
+++ assets/ChangeLog-CSkN-eH1.js
@@ -1,4 +1,4 @@
-import{j as n}from"./jsx-runtime-DEdD30eg.js";import{useMDXComponents as r}from"./index-CcnH5Kt0.js";import{ae as i,am as m}from"./index-B8n5Ve1E.js";import"./index-RYns6xqu.js";import"./iframe-BGYP1khF.js";import"../sb-preview/runtime.js";import"./index-sbqOYYIm.js";import"./index-BwO1pRjM.js";import"./index-DrFu-skq.js";const a=`## V1.2.9 (2024/12/4)
+import{j as n}from"./jsx-runtime-DEdD30eg.js";import{useMDXComponents as r}from"./index-CcnH5Kt0.js";import{ae as i,am as m}from"./index-DzuLIRLw.js";import"./index-RYns6xqu.js";import"./iframe-DTabg9h9.js";import"../sb-preview/runtime.js";import"./index-sbqOYYIm.js";import"./index-BwO1pRjM.js";import"./index-DrFu-skq.js";const a=`## V1.2.9 (2024/12/4)
 
 ✨ New Feature
 
