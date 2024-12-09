21f24b49b8cb98fa7fadab67651fa81eafb4673b
Kevin <kk2229.yang@gmail.com>
2024-12-09 10:01:11 +0800

    Updates

---
+++ assets/accordion.stories-C2T_5CCN.js
@@ -0,0 +1,11 @@
+import{j as s}from"./jsx-runtime-DEdD30eg.js";import"./title-DjADW0si.js";import"./button-DUOlbcAA.js";import"./checkbox-BbQOm0KL.js";import"./input-BWkpi1yj.js";import"./textarea-jggigcCy.js";import"./toggle-CEWviFwF.js";import"./radio-wVtPZKvI.js";import{A as o}from"./stepper-L0ihGmRm.js";import"./breadcrumb-BjVYaTeR.js";import"./progress-circle-DxITm_MT.js";import"./progress-line-BS6M-A1S.js";import"./styled-7_Fd9YFd.js";import"./menu-BAUrcos8.js";import"./image-CZs88QCg.js";import"./toast-4l9AxWm1.js";import"./index-RYns6xqu.js";import"./lodash-CcdATr5u.js";import"./warning-DPjiZ-Sx.js";import"./index-sbqOYYIm.js";import"./offline-Ddz6ifNG.js";const W={title:"Component/Accordion",component:o,tags:["autodocs"],argTypes:{dataSource:{description:"資料來源",table:{category:"PROPS"}},isOpenAll:{description:"是否全部展開",table:{category:"PROPS"}},className:{description:"客製化樣式",table:{category:"PROPS"}}},parameters:{docs:{title:"Accordion",description:{component:"折疊選單組件的呈現及說明。"}}}},c=[{id:"1",label:"Q1. What is React?",detail:"React is a JavaScript library for building user interfaces."},{id:"2",label:"Q2. What are the features of React?",detail:"The features of React include component-based architecture, unidirectional data flow, and virtual DOM."},{id:"3",label:"Q3. What is included in the React ecosystem?",detail:"The React ecosystem includes React Router, Redux, and Axios."},{id:"4",label:"Q4. What are the advantages of React?",detail:"The advantages of React include component-based architecture, unidirectional data flow, and virtual DOM. These features provide efficiency, reusability, and better performance."}],e={name:"預設項目",args:{dataSource:c,isOpenAll:!1,className:""},render(i){return s.jsx(o,{...i})}};var t,a,r;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  name: '預設項目',
+  args: {
+    dataSource: qaList,
+    isOpenAll: false,
+    className: ''
+  },
+  render(args) {
+    return <Accordion {...args} />;
+  }
+}`,...(r=(a=e.parameters)==null?void 0:a.docs)==null?void 0:r.source}}};const j=["Default"];export{e as Default,j as __namedExportsOrder,W as default};
