0c91c5dc8b8fc435254baff7dec281e144e5e6a3
Kevin <kk2229.yang@gmail.com>
2024-12-13 11:23:59 +0800

    Updates

---
+++ assets/accordion.stories-CMO3nT4O.js
@@ -0,0 +1,11 @@
+import{j as s}from"./jsx-runtime-DEdD30eg.js";import"./title-DgI6XFGL.js";import"./button-CILfgu0O.js";import"./checkbox-CBMRj_Ik.js";import"./input-B1f2GAOq.js";import"./textarea-jggigcCy.js";import"./toggle--rC9d8jO.js";import"./radio-BrflwewT.js";import{A as o}from"./stepper-D20rdR0e.js";import"./breadcrumb-BUHvKToT.js";import"./progress-circle-B8_-B84j.js";import"./progress-line-BcO7QhX8.js";import"./styled-7_Fd9YFd.js";import"./menu-BfpC7pJY.js";import"./image-CZs88QCg.js";import"./toast-CB_i8VjC.js";import"./index-RYns6xqu.js";import"./lodash-CcdATr5u.js";import"./string-bv4IUxbB.js";import"./warning-DPjiZ-Sx.js";import"./index-sbqOYYIm.js";import"./offline-Ddz6ifNG.js";const j={title:"Component/Accordion",component:o,tags:["autodocs"],argTypes:{dataSource:{description:"資料來源",table:{category:"PROPS"}},isOpenAll:{description:"是否全部展開",table:{category:"PROPS"}},className:{description:"客製化樣式",table:{category:"PROPS"}}},parameters:{docs:{title:"Accordion",description:{component:"折疊選單組件的呈現及說明。"}}}},c=[{id:"1",label:"Q1. What is React?",detail:"React is a JavaScript library for building user interfaces."},{id:"2",label:"Q2. What are the features of React?",detail:"The features of React include component-based architecture, unidirectional data flow, and virtual DOM."},{id:"3",label:"Q3. What is included in the React ecosystem?",detail:"The React ecosystem includes React Router, Redux, and Axios."},{id:"4",label:"Q4. What are the advantages of React?",detail:"The advantages of React include component-based architecture, unidirectional data flow, and virtual DOM. These features provide efficiency, reusability, and better performance."}],e={name:"預設項目",args:{dataSource:c,isOpenAll:!1,className:""},render(i){return s.jsx(o,{...i})}};var t,a,r;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  name: '預設項目',
+  args: {
+    dataSource: qaList,
+    isOpenAll: false,
+    className: ''
+  },
+  render(args) {
+    return <Accordion {...args} />;
+  }
+}`,...(r=(a=e.parameters)==null?void 0:a.docs)==null?void 0:r.source}}};const N=["Default"];export{e as Default,N as __namedExportsOrder,j as default};
---
+++ assets/checkbox.stories-tJRyFKvD.js
@@ -0,0 +1,77 @@
+import{j as o}from"./jsx-runtime-DEdD30eg.js";import{a as u}from"./chunk-454WOBUV-CM0pFb8Z.js";import{C as r}from"./checkbox-CBMRj_Ik.js";import"./index-RYns6xqu.js";import"./v4-CQkTLCs1.js";import"./warning-DPjiZ-Sx.js";const g=[{label:"Option1",value:"option1",isDisabled:!1},{label:"Option2",value:"option2",isDisabled:!1},{label:"Option3",value:"option3",isDisabled:!1}],P={title:"Component/Checkbox",component:r,tags:["autodocs"],argTypes:{themeColor:{description:"主題顏色",control:{type:"select",options:["primary","secondary","neutral","info","success","warning","error"]},table:{category:"PROPS"}},dataSource:{description:"資料來源",table:{category:"PROPS"},required:!0},initValue:{description:"選中的項目",table:{category:"PROPS"}},direction:{description:"排列方向",control:{type:"select",options:["row","column"]},table:{category:"PROPS"}},className:{description:"客製化樣式",table:{category:"PROPS"}},onChange:{description:"選中選項改變時的回調函數",action:"onChange",table:{category:"EVENTS"}}},args:{themeColor:"primary",dataSource:g,initValue:["option1","option3"],direction:"row",className:"",onChange:e=>u("onChange")(e)},parameters:{docs:{title:"Checkbox",description:{component:"複選框組件的呈現及說明。"}}}},k=[{label:"Option1",value:"option1",isDisabled:!1},{label:"Option2",value:"option2",isDisabled:!1},{label:"Option3",value:"option3",isDisabled:!0}],a={name:"預設項目",args:{dataSource:k},render(e){return o.jsx(r,{...e})}},t={name:"排列方向",args:{},parameters:{docs:{source:{transform(e,x){const{args:b}=x;return`
+<Radio {...args} direction=${b.direction}/>
+`}}}},render(e){return o.jsx(r,{...e})}},s={name:"主題色彩",args:{dataSource:g,initValue:["option1"],className:"",onChange:e=>u("onChange")(e)},parameters:{docs:{source:{code:`
+<Checkbox {...args} themeColor="neutral" />
+<Checkbox {...args} themeColor="primary" />
+<Checkbox {...args} themeColor="secondary" />
+<Checkbox {...args} themeColor="info" />
+<Checkbox {...args} themeColor="success" />
+<Checkbox {...args} themeColor="warning" />
+<Checkbox {...args} themeColor="error" />
+        `}}},render(e){return o.jsxs("div",{style:{display:"flex",flexDirection:"column",gap:"16px"},children:[o.jsx(r,{...e,themeColor:"primary"}),o.jsx(r,{...e,themeColor:"secondary"}),o.jsx(r,{...e,themeColor:"neutral"}),o.jsx(r,{...e,themeColor:"info"}),o.jsx(r,{...e,themeColor:"success"}),o.jsx(r,{...e,themeColor:"warning"}),o.jsx(r,{...e,themeColor:"error"})]})}};var n,c,i;a.parameters={...a.parameters,docs:{...(n=a.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  name: '預設項目',
+  args: {
+    dataSource: defaultOptions
+  },
+  render(args) {
+    return <Checkbox {...args} />;
+  }
+}`,...(i=(c=a.parameters)==null?void 0:c.docs)==null?void 0:i.source}}};var l,m,p;t.parameters={...t.parameters,docs:{...(l=t.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  name: '排列方向',
+  args: {},
+  parameters: {
+    docs: {
+      source: {
+        transform(code: string, storyContext: StoryContext) {
+          const {
+            args
+          } = storyContext;
+          return \`
+<Radio {...args} direction=\${args.direction}/>
+\`;
+        }
+      }
+    }
+  },
+  render(args) {
+    return <Checkbox {...args} />;
+  }
+}`,...(p=(m=t.parameters)==null?void 0:m.docs)==null?void 0:p.source}}};var d,C,h;s.parameters={...s.parameters,docs:{...(d=s.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  name: '主題色彩',
+  args: {
+    dataSource: options,
+    initValue: ['option1'],
+    className: '',
+    onChange: e => action('onChange')(e)
+  },
+  parameters: {
+    docs: {
+      source: {
+        code: \`
+<Checkbox {...args} themeColor="neutral" />
+<Checkbox {...args} themeColor="primary" />
+<Checkbox {...args} themeColor="secondary" />
+<Checkbox {...args} themeColor="info" />
+<Checkbox {...args} themeColor="success" />
+<Checkbox {...args} themeColor="warning" />
+<Checkbox {...args} themeColor="error" />
+        \`
+      }
+    }
+  },
+  render(args) {
+    return <div style={{
+      display: 'flex',
+      flexDirection: 'column',
+      gap: '16px'
+    }}>
+        <Checkbox {...args} themeColor="primary" />
+        <Checkbox {...args} themeColor="secondary" />
+        <Checkbox {...args} themeColor="neutral" />
+        <Checkbox {...args} themeColor="info" />
+        <Checkbox {...args} themeColor="success" />
+        <Checkbox {...args} themeColor="warning" />
+        <Checkbox {...args} themeColor="error" />
+      </div>;
+  }
+}`,...(h=(C=s.parameters)==null?void 0:C.docs)==null?void 0:h.source}}};const v=["Default","Direction","Theme"];export{a as Default,t as Direction,s as Theme,v as __namedExportsOrder,P as default};
