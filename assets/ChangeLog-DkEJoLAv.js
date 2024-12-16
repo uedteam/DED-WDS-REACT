acd2eb3d9ed72ff09c333276adc0b049425cdfc1
Kevin <kk2229.yang@gmail.com>
2024-12-16 10:42:01 +0800

    Updates

--- assets/ChangeLog-DkEJoLAv.js
+++ assets/ChangeLog-DkEJoLAv.js
@@ -1,4 +1,4 @@
-import{j as n}from"./jsx-runtime-DEdD30eg.js";import{useMDXComponents as r}from"./index-CcnH5Kt0.js";import{ae as a,am as i}from"./index-BhSCVg4L.js";import"./index-RYns6xqu.js";import"./iframe-5k8Nbfdv.js";import"../sb-preview/runtime.js";import"./index-sbqOYYIm.js";import"./index-BwO1pRjM.js";import"./index-DrFu-skq.js";const s=`## V1.2.12 (2024/12/14)
+import{j as n}from"./jsx-runtime-DEdD30eg.js";import{useMDXComponents as r}from"./index-CcnH5Kt0.js";import{ae as a,am as i}from"./index-Px5cJtnX.js";import"./index-RYns6xqu.js";import"./iframe-DTT56h4-.js";import"../sb-preview/runtime.js";import"./index-sbqOYYIm.js";import"./index-BwO1pRjM.js";import"./index-DrFu-skq.js";const s=`## V1.2.12 (2024/12/14)
 
 ✨ New Feature
 
