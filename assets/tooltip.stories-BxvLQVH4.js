ec5cf89ef92b46cced553f3c2d740fc05bed1b53
Kevin <kk2229.yang@gmail.com>
2024-12-18 10:04:00 +0800

    Updates

---
+++ assets/tooltip.stories-BxvLQVH4.js
@@ -0,0 +1,12 @@
+import{j as o}from"./jsx-runtime-DEdD30eg.js";import"./title-B6FjmO7v.js";import{B as s}from"./toast-pTIqXw8I.js";import"./checkbox-gHzG2Us7.js";import"./input-47lKdKbE.js";import"./textarea-CO8Z8fZG.js";import"./toggle-B_GFje5b.js";import"./radio-BhGOQQ7m.js";import{b as p}from"./styled-CpN1upj1.js";import"./breadcrumb-C8k9_Tpo.js";import"./progress-circle-ReGt0b-K.js";import"./progress-line-DHphvmbz.js";import"./styled-7_Fd9YFd.js";import"./menu-3Z-Smywu.js";import"./image-CZs88QCg.js";import"./status-indicator-C0euK5S5.js";import{a}from"./disable-circle-Qt9C8lli.js";import"./index-RYns6xqu.js";import"./lodash-CcdATr5u.js";import"./string-ECkTvc_B.js";import"./check-BnJBHFpS.js";import"./index-sbqOYYIm.js";import"./offline-Ddz6ifNG.js";const D={title:"Component/Tooltip",component:p,tags:["autodocs"],argTypes:{content:{description:"提示內容",table:{category:"PROPS"}},showArrow:{description:"是否顯示箭頭",table:{category:"PROPS"}},placement:{description:"提示框位置",control:{type:"select",options:["top-left","top","top-right","right-top","right","right-bottom","bottom-right","bottom","bottom-left","left-bottom","left","left-top"]},table:{category:"PROPS"}},className:{description:"客製化樣式",table:{category:"PROPS"}},children:{description:"顯示內容",table:{category:"SLOTS"}}},args:{content:"good day",placement:"top",showArrow:!0,className:"",children:o.jsx(s,{prefix:o.jsx(a,{}),themeColor:"primary",variant:"filled",radius:"md",onClick:()=>({}),children:"Hover Me"})},parameters:{docs:{title:"Tooltip",description:{component:"提示工具組件的呈現及說明。"}}}},t={name:"預設項目",args:{},render(n){return o.jsx("div",{style:{display:"flex",justifyContent:"center"},children:o.jsx(p,{...n})})}};var r,e,i;t.parameters={...t.parameters,docs:{...(r=t.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  name: '預設項目',
+  args: {},
+  render(args) {
+    return <div style={{
+      display: 'flex',
+      justifyContent: 'center'
+    }}>
+        <Tooltip {...args}></Tooltip>
+      </div>;
+  }
+}`,...(i=(e=t.parameters)==null?void 0:e.docs)==null?void 0:i.source}}};const E=["Default"];export{t as Default,E as __namedExportsOrder,D as default};
