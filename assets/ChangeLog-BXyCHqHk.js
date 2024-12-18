94ca885e909534f63a76570ab1a2ed53c682dcb1
Kevin <kk2229.yang@gmail.com>
2024-12-18 10:08:27 +0800

    Updates

---
+++ assets/ChangeLog-BXyCHqHk.js
@@ -0,0 +1,23 @@
+import{j as n}from"./jsx-runtime-DEdD30eg.js";import{useMDXComponents as r}from"./index-CcnH5Kt0.js";import{ae as a,am as i}from"./index-C57bIHFE.js";import"./index-RYns6xqu.js";import"./iframe-GhY_hx-4.js";import"../sb-preview/runtime.js";import"./index-sbqOYYIm.js";import"./index-BwO1pRjM.js";import"./index-DrFu-skq.js";const s=`## V1.2.12 (2024/12/14)
+
+✨ New Feature
+
+---
+
+🆙 Update
+
+- refactor: refactor styling and data handling in checkbox and radio
+  components
+- style: standardize styling across toggle components
+- style: refactor tooltip component styles and functionality
+- refactor: refactor input and textarea components for styling
+  improvements
+- feat: implement UI updates with primary theme color palette
+
+🪲 Bug Fix
+`;function o(t){const e={h1:"h1",...r(),...t.components};return n.jsxs(n.Fragment,{children:[`
+`,`
+`,`
+`,n.jsx(a,{title:"Introduction/Changelog"}),`
+`,n.jsx(e.h1,{id:"changelog",children:"Changelog"}),`
+`,n.jsx(i,{children:s})]})}function x(t={}){const{wrapper:e}={...r(),...t.components};return e?n.jsx(e,{...t,children:n.jsx(o,{...t})}):o(t)}export{x as default};
