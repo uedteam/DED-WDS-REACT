bf874472d891f5b6c518d3bcfa35eea1e7cd3da8
Kevin <kk2229.yang@gmail.com>
2024-12-18 10:03:24 +0800

    Updates

---
+++ assets/search.stories-BredEkcg.js
@@ -0,0 +1,21 @@
+import{j as r}from"./jsx-runtime-DEdD30eg.js";import{a}from"./chunk-454WOBUV-CM0pFb8Z.js";import"./title-BPnjcUZo.js";import"./toast-BEzfIbOE.js";import"./checkbox-gHzG2Us7.js";import"./input-Cqvs4Uxt.js";import"./textarea-CO8Z8fZG.js";import"./toggle-B_GFje5b.js";import"./radio-BhGOQQ7m.js";import{d as o}from"./styled-Rb-jBBh0.js";import"./breadcrumb-b1HMjbI-.js";import"./progress-circle-ReGt0b-K.js";import"./progress-line-DHphvmbz.js";import"./styled-7_Fd9YFd.js";import"./menu-DboPxrNc.js";import"./image-BUf_2wZ-.js";import"./status-indicator-C0euK5S5.js";import"./index-RYns6xqu.js";import"./v4-CQkTLCs1.js";import"./lodash-CcdATr5u.js";import"./disable-circle-Qt9C8lli.js";import"./string-ECkTvc_B.js";import"./check-BnJBHFpS.js";import"./index-sbqOYYIm.js";import"./offline-Ddz6ifNG.js";const A={title:"Component/Search",component:o,tags:["autodocs"],argTypes:{size:{description:"尺寸",control:{type:"select",options:["small","medium","large"]},table:{category:"PROPS"}},isDisabled:{description:"是否禁用",table:{category:"PROPS"}},className:{description:"客製化樣式",table:{category:"PROPS"}},onClick:{description:"點擊事件",table:{category:"EVENTS"}},onChange:{description:"改變事件",table:{category:"EVENTS"}}},args:{size:"medium",isDisabled:!1,className:"",onClick:a("onClick"),onChange:a("onChange")},parameters:{docs:{title:"搜尋",description:{component:"搜尋組件的呈現及說明。"}}}},s={name:"預設項目",args:{},render(e){return r.jsx(o,{...e})}},t={name:"搜尋尺寸",args:{},render(e){return r.jsxs("div",{style:{display:"flex",flexDirection:"column",gap:"10px"},children:[r.jsx(o,{...e,size:"small"}),r.jsx(o,{...e,size:"medium"}),r.jsx(o,{...e,size:"large"})]})}};var i,m,n;s.parameters={...s.parameters,docs:{...(i=s.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  name: '預設項目',
+  args: {},
+  render(args) {
+    return <Search {...args} />;
+  }
+}`,...(n=(m=s.parameters)==null?void 0:m.docs)==null?void 0:n.source}}};var p,c,l;t.parameters={...t.parameters,docs:{...(p=t.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  name: '搜尋尺寸',
+  args: {},
+  render(args) {
+    return <div style={{
+      display: 'flex',
+      flexDirection: 'column',
+      gap: '10px'
+    }}>
+        <Search {...args} size="small" />
+        <Search {...args} size="medium" />
+        <Search {...args} size="large" />
+      </div>;
+  }
+}`,...(l=(c=t.parameters)==null?void 0:c.docs)==null?void 0:l.source}}};const B=["Default","Size"];export{s as Default,t as Size,B as __namedExportsOrder,A as default};
---
+++ assets/slider.stories-CbRE0Kvk.js
@@ -0,0 +1,50 @@
+import{j as r}from"./jsx-runtime-DEdD30eg.js";import{a as d}from"./chunk-454WOBUV-CM0pFb8Z.js";import"./title-BPnjcUZo.js";import"./toast-BEzfIbOE.js";import"./checkbox-gHzG2Us7.js";import"./input-Cqvs4Uxt.js";import"./textarea-CO8Z8fZG.js";import"./toggle-B_GFje5b.js";import"./radio-BhGOQQ7m.js";import{S as i}from"./styled-Rb-jBBh0.js";import"./breadcrumb-b1HMjbI-.js";import"./progress-circle-ReGt0b-K.js";import"./progress-line-DHphvmbz.js";import"./styled-7_Fd9YFd.js";import"./menu-DboPxrNc.js";import"./image-BUf_2wZ-.js";import"./status-indicator-C0euK5S5.js";import"./index-RYns6xqu.js";import"./v4-CQkTLCs1.js";import"./lodash-CcdATr5u.js";import"./disable-circle-Qt9C8lli.js";import"./string-ECkTvc_B.js";import"./check-BnJBHFpS.js";import"./index-sbqOYYIm.js";import"./offline-Ddz6ifNG.js";const A={title:"Component/Slider",component:i,tags:["autodocs"],argTypes:{themeColor:{description:"主題顏色",control:{type:"select",options:["primary","secondary","neutral","info","success","warning","error"]},table:{category:"PROPS"}},max:{description:"最大值",table:{category:"PROPS"}},min:{description:"最小值",table:{category:"PROPS"}},label:{description:"顯示單位",table:{category:"PROPS"}},step:{description:"步進值",table:{category:"PROPS"}},initValue:{description:"初始值",table:{category:"PROPS"}},isDisabled:{description:"是否禁用",table:{category:"PROPS"}},className:{description:"客製化樣式",table:{category:"PROPS"}},onChange:{description:"值改變事件",action:"onChange",table:{category:"EVENTS"}}},parameters:{docs:{title:"Slider",description:{component:"滑桿組件的呈現及說明。"}}},args:{themeColor:"primary",min:-100,max:100,step:1,initValue:0,label:"℃",isDisabled:!1,onChange:d("onChange"),className:""}},o={name:"預設項目",args:{},render(e){return r.jsx(i,{...e})}},t={name:"主題色彩",args:{min:0,max:100,step:1,initValue:10,label:"%"},parameters:{docs:{source:{code:`
+<Slider {...args} initValue={40} themeColor="primary" />
+<Slider {...args} initValue={50} themeColor="secondary" />
+<Slider {...args} initValue={60} themeColor="neutral" />
+<Slider {...args} initValue={70} themeColor="info" />
+<Slider {...args} initValue={80} themeColor="success" />
+<Slider {...args} initValue={90} themeColor="warning" />
+<Slider {...args} initValue={100} themeColor="error" />
+`}}},render(e){return r.jsxs("div",{children:[r.jsx(i,{...e,initValue:40,themeColor:"primary"}),r.jsx(i,{...e,initValue:50,themeColor:"secondary"}),r.jsx(i,{...e,initValue:60,themeColor:"neutral"}),r.jsx(i,{...e,initValue:70,themeColor:"info"}),r.jsx(i,{...e,initValue:80,themeColor:"success"}),r.jsx(i,{...e,initValue:90,themeColor:"warning"}),r.jsx(i,{...e,initValue:100,themeColor:"error"})]})}};var a,l,s;o.parameters={...o.parameters,docs:{...(a=o.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  name: '預設項目',
+  args: {},
+  render(args) {
+    return <Slider {...args} />;
+  }
+}`,...(s=(l=o.parameters)==null?void 0:l.docs)==null?void 0:s.source}}};var n,m,c;t.parameters={...t.parameters,docs:{...(n=t.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  name: '主題色彩',
+  args: {
+    min: 0,
+    max: 100,
+    step: 1,
+    initValue: 10,
+    label: '%'
+  },
+  parameters: {
+    docs: {
+      source: {
+        code: \`
+<Slider {...args} initValue={40} themeColor="primary" />
+<Slider {...args} initValue={50} themeColor="secondary" />
+<Slider {...args} initValue={60} themeColor="neutral" />
+<Slider {...args} initValue={70} themeColor="info" />
+<Slider {...args} initValue={80} themeColor="success" />
+<Slider {...args} initValue={90} themeColor="warning" />
+<Slider {...args} initValue={100} themeColor="error" />
+\`
+      }
+    }
+  },
+  render(args) {
+    return <div>
+        <Slider {...args} initValue={40} themeColor="primary" />
+        <Slider {...args} initValue={50} themeColor="secondary" />
+        <Slider {...args} initValue={60} themeColor="neutral" />
+        <Slider {...args} initValue={70} themeColor="info" />
+        <Slider {...args} initValue={80} themeColor="success" />
+        <Slider {...args} initValue={90} themeColor="warning" />
+        <Slider {...args} initValue={100} themeColor="error" />
+      </div>;
+  }
+}`,...(c=(m=t.parameters)==null?void 0:m.docs)==null?void 0:c.source}}};const B=["Default","ThemeColor"];export{o as Default,t as ThemeColor,B as __namedExportsOrder,A as default};
