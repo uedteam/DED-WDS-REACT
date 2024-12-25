5ceb56be582feed8b656fb8b890acec56be37621
Kevin <kk2229.yang@gmail.com>
2024-12-25 12:03:49 +0800

    Updates

---
+++ assets/card.stories-D65Vm80H.js
@@ -0,0 +1,89 @@
+import{j as e}from"./jsx-runtime-DEdD30eg.js";import{a as j}from"./chunk-454WOBUV-CM0pFb8Z.js";import{T as l}from"./title-C0o4P0Zd.js";import{B as o}from"./toast-Bo4Irzar.js";import"./checkbox-Dx9j1Oee.js";import"./input-CueOdQnq.js";import"./textarea-CO8Z8fZG.js";import"./toggle-Ck5Sa1oo.js";import"./radio-CMOPLxLt.js";import{h as r,C as s}from"./navbar-L0Y4wvFE.js";import"./breadcrumb-ByLCAha8.js";import"./progress-circle-wFm81-mr.js";import"./progress-line-DG1O7uhi.js";import"./menu-BdrCRtAY.js";import"./image-CZs88QCg.js";import"./status-indicator-BDww4a8h.js";import"./badge-vsbKzH_B.js";import{G as m,R as u,C as i}from"./grid-C0v7SZy_.js";import{A as c}from"./disable-circle-Qt9C8lli.js";import"./index-RYns6xqu.js";import"./v4-CQkTLCs1.js";import"./string-BqrfsRv8.js";import"./check-BnJBHFpS.js";import"./index-sbqOYYIm.js";import"./navigate-arrow-6VIhmLdI.js";import"./notification-NM2mg_B2.js";const W={title:"Component/Card",component:r,tags:["autodocs"],argTypes:{hasHeaderDivider:{description:"是否有標題分隔線",table:{category:"PROPS"}},className:{description:"客製化樣式",table:{category:"PROPS"}},cardHeader:{description:"卡片標題",table:{category:"SLOTS"}},children:{description:"卡片內容",table:{category:"SLOTS"}},cardFooter:{description:"卡片底部",table:{category:"SLOTS"}},onClick:{action:"clicked",description:"點擊事件",table:{category:"EVENTS"}}},parameters:{docs:{title:"Card",description:{component:"卡片組件的呈現及說明。"}}},args:{hasHeaderDivider:!1,className:"",cardHeader:"",children:"",cardFooter:"",onClick:j("onClick")}},a={name:"預設項目",args:{cardHeader:e.jsx(l,{themeColor:"primary",level:2,children:"Header Title"}),cardFooter:e.jsx(o,{themeColor:"primary",variant:"filled",suffix:e.jsx(c,{}),onClick:()=>({}),children:"Button"})},render:function(t){return e.jsx(m,{children:e.jsx(u,{children:e.jsx(i,{xs:12,md:6,lg:4,children:e.jsx(r,{...t,children:e.jsx(s,{hasBorder:!1,imgSrc:"libs/src/assets/card_bg.png",align:"left",title:"Card Title",subtitle:"Subtitle",description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s..."})})})})})}},n={name:"垂直佈局",args:{cardHeader:e.jsx(l,{themeColor:"primary",level:2,children:"Header Title"}),cardFooter:e.jsx(o,{themeColor:"primary",variant:"filled",suffix:e.jsx(c,{}),onClick:()=>({}),children:"Button"})},render:function(t){return e.jsx(m,{children:e.jsxs(u,{children:[e.jsx(i,{xs:12,md:6,lg:3,children:e.jsx(r,{...t,children:e.jsx(s,{hasBorder:!1,imgSrc:"libs/src/assets/card_bg.png",align:"left",title:"Card Title",subtitle:"Subtitle",description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s..."})})}),e.jsx(i,{xs:12,md:6,lg:3,children:e.jsx(r,{...t,children:e.jsx(s,{hasBorder:!1,imgSrc:"libs/src/assets/card_bg.png",align:"left",title:"Card Title",subtitle:"Subtitle",description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s..."})})}),e.jsx(i,{xs:12,md:6,lg:3,children:e.jsx(r,{...t,children:e.jsx(s,{hasBorder:!1,imgSrc:"libs/src/assets/card_bg.png",align:"left",title:"Card Title",subtitle:"Subtitle",description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s..."})})}),e.jsx(i,{xs:12,md:6,lg:3,children:e.jsx(r,{...t,children:e.jsx(s,{hasBorder:!1,imgSrc:"libs/src/assets/card_bg.png",align:"left",title:"Card Title",subtitle:"Subtitle",description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s..."})})})]})})}},d={name:"水平佈局",args:{cardHeader:e.jsx(l,{themeColor:"primary",level:2,children:"Header Title"}),cardFooter:e.jsx(o,{themeColor:"primary",variant:"filled",size:"large",suffix:e.jsx(c,{}),onClick:()=>({}),children:"Button"})},render:function(t){return e.jsx(m,{children:e.jsxs(u,{children:[e.jsx(i,{xs:12,children:e.jsx(r,{...t,children:e.jsx(s,{hasBorder:!1,layout:"horizontal",imgSrc:"libs/src/assets/card_bg.png",align:"left",title:"Card Title",subtitle:"Subtitle",description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s..."})})}),e.jsx(i,{xs:12,children:e.jsx(r,{...t,children:e.jsx(s,{hasBorder:!1,layout:"horizontal",imgSrc:"libs/src/assets/card_bg.png",align:"left",title:"Card Title",subtitle:"Subtitle",description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s..."})})}),e.jsx(i,{xs:12,children:e.jsx(r,{...t,children:e.jsx(s,{hasBorder:!1,layout:"horizontal",imgSrc:"libs/src/assets/card_bg.png",align:"left",title:"Card Title",subtitle:"Subtitle",description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s..."})})})]})})}};var p,g,h;a.parameters={...a.parameters,docs:{...(p=a.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  name: '預設項目',
+  args: {
+    cardHeader: <Title themeColor="primary" level={2}>
+        Header Title
+      </Title>,
+    cardFooter: <Button themeColor="primary" variant={'filled'} suffix={<ArrowDownIcon />} onClick={() => ({})}>
+        Button
+      </Button>
+  },
+  render: function (args) {
+    return <Grid>
+        <Row>
+          <Column xs={12} md={6} lg={4}>
+            <Card {...args}>
+              <CardSimple hasBorder={false} imgSrc="libs/src/assets/card_bg.png" align="left" title="Card Title" subtitle="Subtitle" description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s..." />
+            </Card>
+          </Column>
+        </Row>
+      </Grid>;
+  }
+}`,...(h=(g=a.parameters)==null?void 0:g.docs)==null?void 0:h.source}}};var y,x,b;n.parameters={...n.parameters,docs:{...(y=n.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  name: '垂直佈局',
+  args: {
+    cardHeader: <Title themeColor="primary" level={2}>
+        Header Title
+      </Title>,
+    cardFooter: <Button themeColor="primary" variant={'filled'} suffix={<ArrowDownIcon />} onClick={() => ({})}>
+        Button
+      </Button>
+  },
+  render: function (args) {
+    return <Grid>
+        <Row>
+          <Column xs={12} md={6} lg={3}>
+            <Card {...args}>
+              <CardSimple hasBorder={false} imgSrc="libs/src/assets/card_bg.png" align="left" title="Card Title" subtitle="Subtitle" description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s..." />
+            </Card>
+          </Column>
+          <Column xs={12} md={6} lg={3}>
+            <Card {...args}>
+              <CardSimple hasBorder={false} imgSrc="libs/src/assets/card_bg.png" align="left" title="Card Title" subtitle="Subtitle" description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s..." />
+            </Card>
+          </Column>
+          <Column xs={12} md={6} lg={3}>
+            <Card {...args}>
+              <CardSimple hasBorder={false} imgSrc="libs/src/assets/card_bg.png" align="left" title="Card Title" subtitle="Subtitle" description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s..." />
+            </Card>
+          </Column>
+          <Column xs={12} md={6} lg={3}>
+            <Card {...args}>
+              <CardSimple hasBorder={false} imgSrc="libs/src/assets/card_bg.png" align="left" title="Card Title" subtitle="Subtitle" description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s..." />
+            </Card>
+          </Column>
+        </Row>
+      </Grid>;
+  }
+}`,...(b=(x=n.parameters)==null?void 0:x.docs)==null?void 0:b.source}}};var C,f,S;d.parameters={...d.parameters,docs:{...(C=d.parameters)==null?void 0:C.docs,source:{originalSource:`{
+  name: '水平佈局',
+  args: {
+    cardHeader: <Title themeColor="primary" level={2}>
+        Header Title
+      </Title>,
+    cardFooter: <Button themeColor="primary" variant={'filled'} size={'large'} suffix={<ArrowDownIcon />} onClick={() => ({})}>
+        Button
+      </Button>
+  },
+  render: function (args) {
+    return <Grid>
+        <Row>
+          <Column xs={12}>
+            <Card {...args}>
+              <CardSimple hasBorder={false} layout="horizontal" imgSrc="libs/src/assets/card_bg.png" align="left" title="Card Title" subtitle="Subtitle" description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s..." />
+            </Card>
+          </Column>
+          <Column xs={12}>
+            <Card {...args}>
+              <CardSimple hasBorder={false} layout="horizontal" imgSrc="libs/src/assets/card_bg.png" align="left" title="Card Title" subtitle="Subtitle" description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s..." />
+            </Card>
+          </Column>
+          <Column xs={12}>
+            <Card {...args}>
+              <CardSimple hasBorder={false} layout="horizontal" imgSrc="libs/src/assets/card_bg.png" align="left" title="Card Title" subtitle="Subtitle" description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s..." />
+            </Card>
+          </Column>
+        </Row>
+      </Grid>;
+  }
+}`,...(S=(f=d.parameters)==null?void 0:f.docs)==null?void 0:S.source}}};const X=["Default","Vertical","Horizontal"];export{a as Default,d as Horizontal,n as Vertical,X as __namedExportsOrder,W as default};
