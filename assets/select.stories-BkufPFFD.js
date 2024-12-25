5f16dc601bef779534675d21143b1eac5a6ef441
Kevin <kk2229.yang@gmail.com>
2024-12-25 22:54:40 +0800

    Updates

---
+++ assets/select.stories-BkufPFFD.js
@@ -0,0 +1,12 @@
+import{j as p}from"./jsx-runtime-DEdD30eg.js";import"./title-C0o4P0Zd.js";import"./toast-Bo4Irzar.js";import"./checkbox-Dx9j1Oee.js";import"./input-CueOdQnq.js";import"./textarea-CO8Z8fZG.js";import"./toggle-Ck5Sa1oo.js";import"./radio-CMOPLxLt.js";import{S as s}from"./side-nav-CBcngig9.js";import"./breadcrumb-ByLCAha8.js";import"./progress-circle-wFm81-mr.js";import"./progress-line-DG1O7uhi.js";import"./menu-BkHlZ5yp.js";import"./image-CZs88QCg.js";import"./status-indicator-BDww4a8h.js";import"./badge-vsbKzH_B.js";import"./layout-DbUmifqy.js";import"./index-RYns6xqu.js";import"./disable-circle-Qt9C8lli.js";import"./string-BqrfsRv8.js";import"./check-BnJBHFpS.js";import"./index-sbqOYYIm.js";import"./logout-Cs4pFqLv.js";import"./notification-NM2mg_B2.js";const a=[{label:"Option 1",value:"1"},{label:"Option 2",value:"2"},{label:"Option 3",value:"3"}],T={title:"Component/Select",component:s,tags:["autodocs"],argTypes:{options:{description:"選項",control:{type:"object"}},placeholder:{description:"預設文字",control:{type:"text"}},value:{description:"值",control:{type:"text"}},isDisabled:{description:"是否禁用",control:{type:"boolean"}},className:{description:"客製化樣式",control:{type:"text"}},onClick:{description:"點擊事件"}},args:{options:a,placeholder:"Select",value:"",isDisabled:!1,className:""},parameters:{docs:{title:"Select",description:{component:"選擇組件的呈現及說明。"}}}},e={name:"預設項目",args:{className:""},render(i){return p.jsx(s,{...i})}};var t,o,r;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  name: '預設項目',
+  args: {
+    /* 設定參數預設值 */
+    // size: 'medium',
+    // isDisabled: false,
+    className: ''
+  },
+  render(args) {
+    return <Select {...args} />;
+  }
+}`,...(r=(o=e.parameters)==null?void 0:o.docs)==null?void 0:r.source}}};const q=["Default"];export{e as Default,q as __namedExportsOrder,T as default};
