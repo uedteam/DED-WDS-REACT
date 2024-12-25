ab8605729e324989a157a16add5c6f944c323079
Kevin <kk2229.yang@gmail.com>
2024-12-25 23:37:28 +0800

    Updates

--- assets/navbar.stories-DZ9PGWER.js
+++
@@ -1,9 +0,0 @@
-import{j as i}from"./jsx-runtime-DEdD30eg.js";import{N as a}from"./side-nav-mDrjcL9V.js";import"./index-RYns6xqu.js";import"./disable-circle-Qt9C8lli.js";import"./logout-Cs4pFqLv.js";import"./title-C0o4P0Zd.js";import"./toast-C2I02jxk.js";import"./string-BqrfsRv8.js";import"./checkbox-Dx9j1Oee.js";import"./check-BnJBHFpS.js";import"./input-CsvIf6m6.js";import"./index-sbqOYYIm.js";import"./textarea-CO8Z8fZG.js";import"./toggle-Ck5Sa1oo.js";import"./radio-CMOPLxLt.js";import"./breadcrumb-DrXwihe_.js";import"./progress-circle-wFm81-mr.js";import"./progress-line-DG1O7uhi.js";import"./menu-DYIHftZB.js";import"./image-CZs88QCg.js";import"./status-indicator-BDww4a8h.js";import"./badge-vsbKzH_B.js";import"./layout-DbUmifqy.js";import"./notification-NM2mg_B2.js";const p=[{label:"Solutions",href:"#solutions",order:2},{label:"Products",href:"#products",order:1},{label:"Technologies",href:"#technologies",order:4},{label:"About",href:"#about",order:3}],w={title:"Component/Navbar",component:a,tags:["autodocs"],argTypes:{className:{description:"客製化樣式",control:{type:"text"}},onClick:{description:"點擊事件"}},args:{links:p,logoSrc:"https://storage.googleapis.com/ded-wds-bucket/AUO_LOGO.svg",onSearch:o=>{console.log(o)}},parameters:{docs:{title:"Navbar",description:{component:"Navbar 組件的呈現及說明。"}}}},r={name:"預設項目",args:{className:""},render(o){return i.jsx(a,{...o})}};var t,e,s;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`{
-  name: '預設項目',
-  args: {
-    className: ''
-  },
-  render(args) {
-    return <Navbar {...args} />;
-  }
-}`,...(s=(e=r.parameters)==null?void 0:e.docs)==null?void 0:s.source}}};const G=["Default"];export{r as Default,G as __namedExportsOrder,w as default};
