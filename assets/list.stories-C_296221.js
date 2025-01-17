78833a34d90b47ae824ce11b9b4f811a5df5b46c
Kevin <kk2229.yang@gmail.com>
2025-01-18 01:45:57 +0800

    Updates

---
+++ assets/list.stories-C_296221.js
@@ -0,0 +1,31 @@
+import{j as r}from"./jsx-runtime-DEdD30eg.js";import{a as u}from"./chunk-454WOBUV-CM0pFb8Z.js";import{L as a}from"./pagination-IrUr8gqF.js";import"./badge-vsbKzH_B.js";import"./breadcrumb-TVPJmAu4.js";import"./toast-BzflhhTI.js";import"./checkbox-D-MAZBDN.js";import"./progress-circle-xnhIhzfk.js";import"./progress-line-DsOpskWp.js";import"./divider-ChKDBDeF.js";import"./image-CZs88QCg.js";import"./input-DaD8_zj6.js";import"./radio-CMOPLxLt.js";import"./textarea-CO8Z8fZG.js";import"./title-Cw6Qriwq.js";import"./toggle-Ck5Sa1oo.js";import"./menu-B7btuLIn.js";import"./status-indicator-BDww4a8h.js";import"./layout-ZjbFUAzr.js";import"./tag-C7LkoCGR.js";import"./grid-D9dEAlnk.js";import{b as s}from"./arrow-drop-down-Fqyji-Tj.js";import"./index-RYns6xqu.js";import"./v4-CQkTLCs1.js";import"./string-BqrfsRv8.js";import"./online-EmbACc0H.js";import"./notification-NM2mg_B2.js";import"./index-sbqOYYIm.js";import"./check-BnJBHFpS.js";import"./useCounter-DpfJ3ATo.js";const d=[{content:{label:"Option1",value:"option1",prefix:r.jsx(s,{})}},{content:{label:"Option2",value:"option2",href:"#",prefix:r.jsx(s,{})}},{content:{label:"Option3",value:"option3",href:"#",prefix:r.jsx(s,{})}}],I={title:"Component/List",component:a,tags:["autodocs"],argTypes:{dataSource:{description:"資料來源",table:{category:"PROPS"}},hasOutline:{description:"是否有外框",control:{type:"boolean"},table:{category:"PROPS"}},hasDivider:{description:"是否有分隔線",table:{category:"PROPS"}},className:{description:"客製化樣式",control:{type:"text"},table:{category:"PROPS"}},onSelect:{description:"選擇事件",table:{category:"EVENTS"}}},args:{dataSource:d,hasOutline:!1,hasDivider:!1,className:"",onSelect:u("onSelect")},parameters:{docs:{title:"List",description:{component:"清單組件的呈現及說明。"}}}},t={name:"預設項目",args:{},parameters:{docs:{source:{code:`
+<List {...args} />`}}},render(o){return r.jsx(a,{...o})}},e={name:"選單樣式",args:{},parameters:{docs:{source:{code:`
+<List {...args} hasOutline />`}}},render(o){return r.jsx(a,{...o,hasOutline:!0})}};var n,i,p;t.parameters={...t.parameters,docs:{...(n=t.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  name: '預設項目',
+  args: {},
+  parameters: {
+    docs: {
+      source: {
+        code: \`
+<List {...args} />\`
+      }
+    }
+  },
+  render(args) {
+    return <List {...args} />;
+  }
+}`,...(p=(i=t.parameters)==null?void 0:i.docs)==null?void 0:p.source}}};var c,m,l;e.parameters={...e.parameters,docs:{...(c=e.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  name: '選單樣式',
+  args: {},
+  parameters: {
+    docs: {
+      source: {
+        code: \`
+<List {...args} hasOutline />\`
+      }
+    }
+  },
+  render(args) {
+    return <List {...args} hasOutline />;
+  }
+}`,...(l=(m=e.parameters)==null?void 0:m.docs)==null?void 0:l.source}}};const J=["Default","Menu"];export{t as Default,e as Menu,J as __namedExportsOrder,I as default};
